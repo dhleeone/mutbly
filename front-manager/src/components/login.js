c11d3e808d5d1eb4b49cae1beb7f8e161e9bd74a
dhleeone <dhleeone@gmail.com>
2022-03-28 11:16:35 +0900

    mutbly

--- front-manager/src/components/login.js
+++ front-manager/src/components/login.js
@@ -62,7 +62,7 @@ export default function SignIn() {
 				localStorage.setItem('access_token', res.data.access);
 				localStorage.setItem('refresh_token', res.data.refresh);
 				axiosInstance.defaults.headers['Authorization'] =
-					'JWT ' + localStorage.getItem('access_token');
+					'Bearer ' + localStorage.getItem('access_token');
                 navigate('/');
 				//console.log(res);
 				//console.log(res.data);
